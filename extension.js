991dbcfc3d7c9d680bd5defe5febdcfbd166f344
Maria James <mariajamesv22@gmail.com>
2023-10-22 23:53:29 +0530

    Refactor generateTemplateAction

--- extension.js
+++ extension.js
@@ -1,25 +1,13 @@
-const fs = require('fs');
-const path = require('path');
 const vscode = require('vscode');
 const TextBisonApiService = require('./src/service/TextBisonApiService');
 const TreeViewProvider = require('./src/helper/TreeViewProvider');
-const { getStringifiedPrompt } = require('./src/helper/helpers');
+const {
+   getFilteredPrompt,
+   getStringifiedPrompt,
+   generateTemplate,
+} = require('./src/helper/helpers');
+const { htmlContent } = require('./src/constants');
 const Prompts = require('./src/prompts/Prompts');
-
-const htmlContent = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Document</title>
-    <link rel="stylesheet" href="app.css" />
-</head>
-<body>
-    <script src="app.js"></script>
-</body>
-</html>
-        `;
 
 const { TextBisonApi } = TextBisonApiService();
 
@@ -27,23 +15,7 @@ const activate = async context => {
    let generateTemplateAction = vscode.commands.registerCommand(
       'CodeXpert.generateTemplate',
       async function () {
-         if (!vscode.workspace || !vscode.workspace.workspaceFolders) {
-            return vscode.window.showErrorMessage(
-               'Please open a project folder first',
-            );
-         }
-         let folderPath = vscode.workspace.workspaceFolders[0].uri
-            .toString()
-            .split(':')[1];
-
-         folderPath = folderPath + '/CodeXpert/src/';
-
-         fs.writeFile(path.join(folderPath, 'index.html'), htmlContent, err => {
-            if (err) {
-               return console.log(err);
-            }
-            vscode.window.showWarningMessage('Created boilerplate files!');
-         });
+         generateTemplate(htmlContent, 'index.html');
       },
    );
 
