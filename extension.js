0417638e605598310116558ae90eb9564397f9ff
Maria James <mariajamesv22@gmail.com>
2023-10-23 12:25:59 +0530

    Add showInputBox to accept user input
    - Accept userinput for DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, PORT_NUMBER
    - terminate execution when user presses esc
    - add basic validations for inputs

--- extension.js
+++ extension.js
@@ -11,6 +11,45 @@ const { htmlContent, baseHTML } = require('./src/constants');
 const Prompts = require('./src/prompts/Prompts');
 
 const { TextBisonApi } = TextBisonApiService();
+let validValues = {
+   DB_HOST: null,
+   DB_USER: null,
+   DB_PASSWORD: null,
+   DB_NAME: null,
+   PORT_NUMBER: null,
+};
+const properties = Object.keys(validValues);
+
+const getNextInput = async index => {
+   if (index >= properties.length) {
+      console.log(validValues);
+      return validValues;
+   }
+
+   const propertyName = properties[index];
+
+   const text = await vscode.window.showInputBox({
+      placeHolder: propertyName,
+      validateInput: text => {
+         const regex = /^\d+$/;
+         const isValid = regex.test(text);
+         if (isValid) {
+            validValues[propertyName] = text;
+         }
+         return isValid ? null : 'Invalid input';
+      },
+   });
+
+   if (text !== undefined) {
+      await getNextInput(index + 1);
+   } else {
+      vscode.window.showErrorMessage(
+         'Input sequence cancelled, terminating...',
+      );
+      return undefined;
+   }
+   return validValues;
+};
 
 const activate = async context => {
    let generateTemplateAction = vscode.commands.registerCommand(
@@ -40,24 +79,34 @@ const activate = async context => {
                'Please open a project folder first',
             );
          }
-         const filteredPrompt = getFilteredPrompt(Prompts.CREATE_DOCKER_FILE, {
-            DB_HOST: 'localhost',
-            DB_USER: 'admin',
-            DB_PASSWORD: 'password123',
-            DB_NAME: 'my_database',
-            PORT_NUMBER: 3306,
-         });
 
-         const panel = vscode.window.createWebviewPanel(
-            'samplePrompt',
-            'Sample Prompt',
-            vscode.ViewColumn.One,
-            {},
-         );
-         panel.webview.html = getHTMLContentForPrompt(baseHTML, filteredPrompt);
+         const output = await getNextInput(0);
+         if (output != undefined) {
+            const filteredPrompt = getFilteredPrompt(
+               Prompts.CREATE_DOCKER_FILE,
+               {
+                  DB_HOST: 'localhost',
+                  DB_USER: 'admin',
+                  DB_PASSWORD: 'password123',
+                  DB_NAME: 'my_database',
+                  PORT_NUMBER: 3306,
+               },
+            );
 
-         let inputPrompt = getStringifiedPrompt(filteredPrompt);
-         TextBisonApi(inputPrompt, '.dockerfile');
+            const panel = vscode.window.createWebviewPanel(
+               'samplePrompt',
+               'Sample Prompt',
+               vscode.ViewColumn.One,
+               {},
+            );
+            panel.webview.html = getHTMLContentForPrompt(
+               baseHTML,
+               filteredPrompt,
+            );
+
+            let inputPrompt = getStringifiedPrompt(filteredPrompt);
+            TextBisonApi(inputPrompt, '.dockerfile');
+         }
       },
    );
 
