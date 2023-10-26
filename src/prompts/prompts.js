33c1a192ee2b5cf10a289cf37ac5a57671c62ec7
Maria James <mariajamesv22@gmail.com>
2023-10-26 11:00:08 +0530

    Modify CREATE_DOCKER_FILE prompt

--- src/prompts/prompts.js
+++ src/prompts/prompts.js
@@ -1,19 +1,17 @@
 const Prompts = {
    CREATE_DOCKER_FILE: `
       Project Description:
+      
+      We are developing a web application in ##LANGUAGE##. The application interacts with a ##DBTYPE## database for data management. The required environment variables are ##ENV_VARIABLE_LIST##. The application should expose port ##PORT_NUMBER##. Please create a multi-stage Dockerfile to encapsulate this application.
 
-      We are building a web application that utilizes Golang (version 1.13) as the programming language. The application connects to a PostgreSQL database for data management. The environment variables required for the database connection are DB_HOST, DB_USER, DB_PASSWORD, and DB_NAME. The application exposes port PORT_NUMBER. 
-      Please construct a multi-stage Dockerfile to encapsulate this application.
-      
       Guidelines:
-      1. Use Golang 1.13 as the base image for the first stage.
-      2. Utilize environment variables for the database connection DB_HOST, DB_USER, DB_PASSWORD, and DB_NAME. 
-      3. Expose port PORT_NUMBER to allow external access to the application.
-      4. Import all the necessary Golang 1.13 modules utilized in the project.
-      5. Optimize the Dockerfile for the best practices and efficiency.
-      6. Use the /cmd/server/ path as the entry file to build the project.
-      
-      Please create the Dockerfile keeping the guidelines in mind and ensure that the application runs smoothly within the Docker container.`,
+
+      1. Utilize ##LANGUAGE## as the base image for the first stage.
+      2. Include environment variables  ##ENV_VARIABLE_LIST##.
+      3. Expose port ##PORT_NUMBER## to enable external access to the application.
+      4. Import all the necessary ##LANGUAGE## libraries utilized in the project.
+      5. Use the ##EXECUTEFILEPATH## path as the entry file to build the project.
+      6. Construct the Dockerfile adhering to these guidelines to ensure a smooth application run within the Docker container.`,
 };
 
 module.exports = Prompts;
